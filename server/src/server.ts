b5765e81c78ef8eec115fbe168d1b3d7c05b6bad
NovaPride <novapride27@gmail.com>
2025-11-19 20:33:46 +0300

    fix: render deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { setupGameHandlers } from "./game/game-server.js";
-import { instrument } from "@socket.io/admin-ui";
+// import { instrument } from "@socket.io/admin-ui";
 import { Server } from "socket.io";
 
 const app = express();
@@ -12,15 +12,17 @@ const io = new Server(httpServer, {
       "https://uno-verse.vercel.app/",
       "http://localhost:5173",
       "http://localhost:3030",
+      "*"
     ],
     credentials: true,
   },
 });
+app.get(("/hello"), (req, res) => res.send("Hello World!"))
 
 setupGameHandlers(io);
 
 // херня чисто для socket admin ui
-const adminUI = express();
-adminUI.use(express.static("./node_modules/@socket.io/admin-ui/ui/dist"));
-adminUI.listen(3030);
-instrument(io, { auth: false, mode: "development" });
+// const adminUI = express();
+// adminUI.use(express.static("./node_modules/@socket.io/admin-ui/ui/dist"));
+// adminUI.listen(3030);
+// instrument(io, { auth: false, mode: "development" });
